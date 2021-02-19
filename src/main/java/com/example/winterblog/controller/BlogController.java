17e033dc2ee310bb2886e301fa1ea970ba37792e
Mima <56038155+dr9ken@users.noreply.github.com>
2021-02-19 14:24:14 +0300

    Added list of users

--- src/main/java/com/example/winterblog/controller/BlogController.java
+++ src/main/java/com/example/winterblog/controller/BlogController.java
@@ -3,6 +3,7 @@ package com.example.winterblog.controller;
 import com.example.winterblog.domain.Post;
 import com.example.winterblog.repository.PostDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/example/winterblog/repository/UserDAO.java
+++ src/main/java/com/example/winterblog/repository/UserDAO.java
@@ -3,6 +3,9 @@ package com.example.winterblog.repository;
 import com.example.winterblog.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserDAO extends JpaRepository<User, Long> {
     User findUserByUsername(String username);
+    List<User> findUserByUsernameIsStartingWith(String username);
 }
