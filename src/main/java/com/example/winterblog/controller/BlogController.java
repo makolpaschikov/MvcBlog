35baf4e3384ce302fab191a95de993d40569ef51
Mima <56038155+dr9ken@users.noreply.github.com>
2021-02-24 02:09:10 +0300

    Added javadoc

--- src/main/java/com/example/winterblog/controller/BlogController.java
+++ src/main/java/com/example/winterblog/controller/BlogController.java
@@ -5,10 +5,12 @@ import com.example.winterblog.domain.User;
 import com.example.winterblog.domain.UserRole;
 import com.example.winterblog.repository.PostDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collections;
 import java.util.List;
